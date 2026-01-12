21bf4f1bf5060ca7b4ab29b226181e4bc1e0391f
isaias tech <isaiasbatistapy.com>
2026-01-11 22:46:44 -0300

    style (sem mudar lógica): ajustando asset das views

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,26 +1,10 @@
 @extends('site.layouts.basico')
 @section('titulo', $titulo)
 @section('conteudo')
-        <div class="topo">
-
-            <div class="logo">
-                <img src="img/logo.png">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.principal') }}">Principal</a></li>
-                    <li><a href="{{ route('site.sobre-nos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
-            </div>
-        </div>
-
         <div class="conteudo-pagina">
             <div class="titulo-pagina">
                 <h1>Entre em contato conosco</h1>
             </div>
-
             <div class="informacao-pagina">
                 <div class="contato-principal">
                     <form>
@@ -42,25 +26,6 @@
                         <button type="submit" class="borda-preta">ENVIAR</button>
                     </form>
                 </div>
-            </div>
-        </div>
-
-        <div class="rodape">
-            <div class="redes-sociais">
-                <h2>Redes sociais</h2>
-                <img src="img/facebook.png">
-                <img src="img/linkedin.png">
-                <img src="img/youtube.png">
-            </div>
-            <div class="area-contato">
-                <h2>Contato</h2>
-                <span>(11) 3333-4444</span>
-                <br>
-                <span>supergestao@dominio.com.br</span>
-            </div>
-            <div class="localizacao">
-                <h2>Localização</h2>
-                <img src="img/mapa.png">
             </div>
         </div>
 @endsection
--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,40 +1,24 @@
 @extends('site.layouts.basico')
 @section('titulo', $titulo)
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="img/logo.png">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.principal') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobre-nos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
-
     <div class="conteudo-destaque">
-
         <div class="esquerda">
             <div class="informacoes">
                 <h1>Sistema Super Gestão</h1>
                 <p>Software para gestão empresarial ideal para sua empresa.
                 <p>
                 <div class="chamada">
-                    <img src="/img/check.png">
+                    <img src="{{ asset('img/check.png') }}">
                     <span class="texto-branco">Gestão completa e descomplicada</span>
                 </div>
                 <div class="chamada">
-                    <img src="img/check.png">
+                    <img src="{{ asset('img/check.png') }}">
                     <span class="texto-branco">Sua empresa na nuvem</span>
                 </div>
             </div>
 
             <div class="video">
-                <img src="img/player_video.jpg">
+                <img src="{{ asset('img/player_video.jpg') }}">
             </div>
         </div>
 
