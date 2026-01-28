80702b802ced290e31f0705718d7885d7b84d7eb
isaias tech <isaiasbatistapy.com>
2026-01-28 18:30:44 -0300

    chore:(migrations): Criando migration contato

---
+++ database/migrations/2026_01_28_201638_create_contatos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contatos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', length: 100);
+            $table->string('telefone', length: 20);
+            $table->string('email', length: 50);
+            $table->integer('motivo_contato');
+            $table->text('mensagem');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contatos');
+    }
+};
